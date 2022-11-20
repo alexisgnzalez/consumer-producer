bd322109a59cf92528b21c892c4af6a923c2a5ae
Alexis Gonzalez <1991.alexis@gmail.com>
2022-11-20 12:26:36 -0400

    Created components consumer and producer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsumerComponent } from './consumer/consumer.component';
+import { ProducerComponent } from './producer/producer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsumerComponent,
+    ProducerComponent,
   ],
   imports: [
     BrowserModule,
