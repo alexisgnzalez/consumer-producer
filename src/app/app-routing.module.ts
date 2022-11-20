63d2f097cf668706bb47f0d324b4945ee3485961
Alexis Gonzalez <1991.alexis@gmail.com>
2022-11-20 13:01:15 -0400

    Added routes and installed stompjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ConsumerComponent } from './consumer/consumer.component';
+import { ProducerComponent } from './producer/producer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'consumer', component: ConsumerComponent},
+  {path: 'producer', component: ProducerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
